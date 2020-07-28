333214114acb023f91113f3d52c418eba04d36c4
MishraRudra <rudramishra407@gmail.com>
2020-07-28 09:11:31 +0530

    made changes to Range sum query

--- Java/Range-Sum-Query-Mutable.java
+++ Java/Range-Sum-Query-Mutable.java
@@ -1,67 +1,79 @@
-class NumArray {
-
-    int[] tree;
-int n;
-public NumArray(int[] nums) {
-    if (nums.length > 0) {
-        n = nums.length;
-        tree = new int[n * 2];
-        buildTree(nums);
-    }
-}
-private void buildTree(int[] nums) {
-    for (int i = n, j = 0;  i < 2 * n; i++,  j++)
-        tree[i] = nums[j];
-    for (int i = n - 1; i > 0; --i)
-        tree[i] = tree[i * 2] + tree[i * 2 + 1];
-}
-    
-    
-    void update(int pos, int val) {
-    pos += n;
-    tree[pos] = val;
-    while (pos > 0) {
-        int left = pos;
-        int right = pos;
-        if (pos % 2 == 0) {
-            right = pos + 1;
-        } else {
-            left = pos - 1;
-        }
-        // parent is updated after child is updated
-        tree[pos / 2] = tree[left] + tree[right];
-        pos /= 2;
-    }
-}
-    
-    
-    public int sumRange(int l, int r) {
-    // get leaf with value 'l'
-    l += n;
-    // get leaf with value 'r'
-    r += n;
-    int sum = 0;
-    while (l <= r) {
-        if ((l % 2) == 1) {
-           sum += tree[l];
-           l++;
-        }
-        if ((r % 2) == 0) {
-           sum += tree[r];
-           r--;
-        }
-        l /= 2;
-        r /= 2;
-    }
-    return sum;
-}
-    
-    
-}
-
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray obj = new NumArray(nums);
  * obj.update(i,val);
  * int param_2 = obj.sumRange(i,j);
+ *
+ * Runtime : 11ms
+ * Memory : 45.4MB
+ * Logic : use segment tree for finding out the sum of the array at given range.
  */
+
+
+class NumArray {
+
+    int[] tree;
+    int n;
+
+    public NumArray(int[] nums) {
+        if (nums.length > 0) {
+            n = nums.length;
+            tree = new int[n * 2];
+            buildTree(nums);
+        }
+    }
+    
+    // build the segment tree and store it in array of size 2 * nums[] length
+    // so the nums[] values are stores at leaves of the segment tree.
+    private void buildTree(int[] nums) {
+        for (int i = n, j = 0; i < 2 * n; i++, j++)
+            tree[i] = nums[j];
+        for (int i = n - 1; i > 0; --i)
+            tree[i] = tree[i * 2] + tree[i * 2 + 1];
+    }
+
+    // update the segment tree if new value to be inserted in tree, then update its parent at pos/2 and so on till root
+    void update(int pos, int val) {
+        pos += n;
+        tree[pos] = val;
+        while (pos > 0) {
+            int left = pos;
+            int right = pos;
+            if (pos % 2 == 0) {
+                right = pos + 1;
+            } else {
+                left = pos - 1;
+            }
+            // parent is updated after child is updated
+            tree[pos / 2] = tree[left] + tree[right];
+            pos /= 2;
+        }
+    }
+
+
+    // this is responsible for the sum betweeen given ranges
+    public int sumRange(int l, int r) {
+        // get leaf with value 'l'
+        l += n;
+        // get leaf with value 'r'
+        r += n;
+        int sum = 0;
+        while (l <= r) {
+            if ((l % 2) == 1) {
+                sum += tree[l];
+                l++;
+            }
+            if ((r % 2) == 0) {
+                sum += tree[r];
+                r--;
+            }
+            l /= 2;
+            r /= 2;
+        }
+        return sum;
+    }
+
+
+}
+
+

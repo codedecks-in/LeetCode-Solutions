13be247ff461c9d61f56a3d66b08880b55c6aac2
girishgr8 <girishthatte35@gmail.com>
2020-07-30 14:02:38 +0530

    sum-root-to-leaf-numbers java

--- Java/sum-root-to-leaf-numbers.java
+++ Java/sum-root-to-leaf-numbers.java
@@ -1,9 +1,22 @@
 /**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
- * = left; this.right = right; } }
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ * 
+ *  Runtime: 0 ms
+    Memory Usage: 39.2 MB
  */
+
 class Solution {
     int sum = 0;
 

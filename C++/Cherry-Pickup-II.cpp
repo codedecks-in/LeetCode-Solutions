d200d74b6828f33aa43fc72a9c52cf85ba42f4e3
Muhammadjon Hakimov <hakimov0777@gmail.com>
2020-08-15 08:06:40 +0300

    Uploaded solutions for bunch of hard problems (#30)
    * Uploaded solutions for bunch of hard problems in C++
    
    * Updated README.md
    
    * Changed difficulty of LRU Cache problem

---
+++ C++/Cherry-Pickup-II.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    // Let's run a simple dfs. If we come to the same cell with two robots, let's subtract that value.
+    int dfs(int n, int m, int r, int c1, int c2, unordered_map<pair<int, pair<int, int>>, int> &dp, vector<vector<int>> &grid) {
+        if (c1 < 0 || c1 >= m || c2 < 0 || c2 >= m) {
+            return 0;
+        } else if (r == n) {
+            return 0;
+        } else if (dp.count({r, {c1, c2}}) > 0) {
+            return dp[{r, {c1, c2}}];
+        } else {
+            int current_ceil = grid[r][c1] + grid[r][c2];
+            
+            // Here is that subtraction
+            if (c1 == c2) {
+                current_ceil -= grid[r][c2];
+            }
+            
+            // Running dfs from all possible cells
+            int mx = 0;
+            for (int i = -1; i <= 1; ++i) {
+                for (int j = -1; j <= 1; ++j) {
+                    mx = max(mx, dfs(n, m, r + 1, c1 + i, c2 + j, dp, grid));
+                }
+            }
+            
+            return dp[{r, {c1, c2}}] = current_ceil + mx;
+        }
+    }
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        unordered_map<pair<int, pair<int, int>>, int> dp;
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        return dfs(n, m, 0, 0, m - 1, dp, grid);
+    }
+};
---
+++ C++/Minimum-Number-of-Flips-to-Convert-Binary-Matrix-to-Zero-Matrix.cpp
@@ -0,0 +1,86 @@
+// The main algorithm in this problem is Breadth First Search.
+class Solution {
+private:
+    bool valid(int n, int m, int x, int y) {
+        return (0 <= x && x < n) && (0 <= y && y < m);
+    }
+    
+    string convert(vector<vector<int>> &c, int n, int m) {
+        string s;
+        
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                s += c[i][j] + '0';
+            }
+        }
+        
+        return s;
+    }
+    
+    int get(string s, int i, int j, int n, int m) {
+        return s[i * m + j] - '0';
+    }
+    
+    void set(string &s, int i, int j, int val, int n, int m) {
+        s[i * m + j] = val + '0';
+    }
+
+public:
+    int minFlips(vector<vector<int>>& c) {
+        int n = c.size();
+        int m = c[0].size();
+        
+        map<string, int> dp;
+        map<string, bool> used;
+    
+        vector<int> dx = {-1, 0, 1, 0};
+        vector<int> dy = {0, 1, 0, -1};
+        
+        string cToNum = convert(c, n, m);
+        string zero(n * m, '0');
+        
+        if (cToNum == zero) {
+            return 0;
+        }
+        
+        queue<string> q;
+        q.push(cToNum);
+        
+        dp[cToNum] = 0;
+        
+        while (!q.empty()) {
+            string a = q.front();
+            q.pop();
+            
+            used[a] = true;
+            
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m; ++j) {
+                    string b = a;
+                    
+                    set(b, i, j, 1 - get(b, i, j, n, m), n, m);
+                    
+                    for (int k = 0; k < 4; ++k) {
+                        int x = i + dx[k];
+                        int y = j + dy[k];
+                        
+                        if (valid(n, m, x, y)) {
+                            set(b, x, y, 1 - get(b, x, y, n, m), n, m);
+                        }
+                    }
+                    
+                    if (!used[b]) {
+                        q.push(b);
+                        dp[b] = dp[a] + 1;
+                        
+                        if (b == zero) {
+                            return dp[b];
+                        }
+                    }
+                }
+            }
+        }
+        
+        return -1;
+    }
+};

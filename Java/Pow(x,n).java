4f69ae00e30af2234cd9d4985a2dfc35a02f9c19
KADIVENDI ROHITH <33064613+Kadivendi@users.noreply.github.com>
2020-10-01 23:04:32 +0530

    Create Pow(x,n).java

---
+++ Java/Pow(x,n).java
@@ -0,0 +1,35 @@
+/*
+LeetCode: Pow(x,n)
+Link: https://leetcode.com/problems/powx-n/
+
+problem: To caluculate x^n
+
+Constraints:
+-100.0 < x < 100.0
+-2^31 <= n <= (2^31)-1
+-10^4 <= xn <= 10^4
+
+Difficulty: Medium
+Time Complexity: Log(n)
+Space Complexity: Log(n)
+
+*/ 
+class Solution {
+    // To caluculate x^n
+    public double myPow(double x, int n) {
+        if(n==0)
+            return 1.0;
+        if(n==1)
+            return x;
+        //if power is negative just inverse it    
+        if(n<0)
+            return 1.0/(x*myPow(x,-(n+1)));
+        //for even power    
+        if(n%2==0)
+            return myPow(x*x,n/2);
+        //for odd power
+        else
+            return x*(myPow(x*x,n/2));
+        
+    }
+}

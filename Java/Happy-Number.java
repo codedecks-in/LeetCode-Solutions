c2734653b5b7d77bfcdabc86a549df740b6a7e35
ifenil <ddobariya001@gmail.com>
2020-10-02 19:45:58 +0530

    Solution without HashSet

--- Java/Happy-Number.java
+++ Java/Happy-Number.java
@@ -1,24 +1,31 @@
-class Solution {
+class HappyNumber {
     public boolean isHappy(int n) {
-        
-        Set<Integer> unique = new HashSet<Integer>();
-
-        while (unique.add(n))
-        {
-            int value = 0;
-            while (n > 0)
-            {
-                value += Math.pow(n % 10, 2);
-                n = n/10;
-            }
-            n = value;
-        }
-
+        int num1, num2; 
+   
+    num1 = num2 = n; 
+    do
+    { 
+        num1 = SquareSum(num1); 
+   
          
-        if(n == 1)
-            return true;
-        else
-            return false;
-        
-   }
+        num2 = SquareSum(SquareSum(num2)); 
+   
+    } 
+    while (num1 != num2); 
+   
+    return (num1 == 1); 
+} 
+
+
+static int SquareSum(int n) 
+    { 
+        int square = 0; 
+        while (n!= 0) 
+        { 
+            square += (n % 10) * (n % 10); 
+            n /= 10; 
+        } 
+    return square; 
+    } 
+ 
 }

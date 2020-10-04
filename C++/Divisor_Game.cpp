879b9f2367a39e7722ada2e308a52b717de9219f
Dipesh Kumar <deepesh.97kumar@gmail.com>
2020-10-04 15:31:16 +0530

    Add comment for better understanding

--- C++/Divisor_Game.cpp
+++ C++/Divisor_Game.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool divisorGame(int N) {
+        // initialize count to zero
         int count =0;
+        // every time one play a move we will start from zero
         for(int i=1;i<N;){
             if(N%i == 0){
                 N -=i;
                     count++;
             }
         }
+        // If it's even Bob will win, as bob move in even
         if(count % 2 == 0)
             return false;
         else

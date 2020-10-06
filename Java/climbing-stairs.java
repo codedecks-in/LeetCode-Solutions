beedb1ee32287dec75f5571e23aa9599209457b5
Pranay2516 <pranaychauhan2002@gmail.com>
2020-10-06 15:51:13 +0530

    70. Climbing Stairs(Java)

---
+++ Java/climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}

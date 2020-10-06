fe0b4585b80fe281b9917c4aac1d00e54324e652
Pranay Chauhan <52269813+PranayChauhan2516@users.noreply.github.com>
2020-10-06 17:24:25 +0530

    Optimising 70. Climbing Stairs to O(1) space

--- Java/climbing-stairs.java
+++ Java/climbing-stairs.java
@@ -1,14 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
+        int prev1=1, prev2=2, cur=0;
+        if(n<=2) return n;
+        for(int i=3;i<=n;++i) {
+            cur=prev1+prev2;
+            prev1=prev2;
+            prev2=cur;
         }
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
+        return cur;
     }
 }

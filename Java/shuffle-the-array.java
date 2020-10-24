945a245075e46c95086c86e7b6f87b8c03102cae
Hamboozy <hamza.bahamdan@gmail.com>
2020-10-23 23:21:10 -0500

    Modified shuffle-the-array.java

--- Java/shuffle-the-array.java
+++ Java/shuffle-the-array.java
@@ -2,9 +2,13 @@ class Solution {
     public int[] shuffle(int[] nums, int n) {
         int [] result = new int[2 * n];
         
+        // Looping over the array
         for(int i = 0; i < 2 * n; i ++){
+            // If the array index is even
             if(i % 2 == 0)
                 result[i] = nums[i / 2];
+
+            // If it's not even, its odd
             else
                 result[i] = nums[n + i / 2];
         }

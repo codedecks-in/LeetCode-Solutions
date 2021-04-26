1745d8f81a167ebd211bbe16304acd779f70de36
garvitdoda1266 <61387258+garvitdoda1266@users.noreply.github.com>
2021-04-26 23:31:41 +0530

    improved c++ code (#161)

--- C++/two-sum.cpp
+++ C++/two-sum.cpp
@@ -15,3 +15,27 @@ public:
         return v;
     }
 };
+
+Above solution is of O(n^2) time complexity
+
+//O(n) time complexity 
+//O(n) space complexity
+
+
+vector<int> twoSum(vector<int> &numbers, int target)
+{
+    //Key is the number and value is its index in the vector.
+	unordered_map<int, int> hash;
+      //  make a vector final result
+	vector<int> finalresult;
+	for (int i = 0; i < numbers.size(); i++) {
+		int n = target - numbers[i];
+
+            //if n is found in map, return them
+		if (hash.find(n) != hash.end()) {
+			result.push_back(hash[n] + 1);
+			result.push_back(i + 1);			
+			return finalresult;
+		}
+}
+}

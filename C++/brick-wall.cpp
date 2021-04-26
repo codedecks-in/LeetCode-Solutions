18eb266ce47f9f5b518e4c960818c3c8b2e1f44a
Rohin Srivastava <sri.rohin184@gmail.com>
2021-04-27 00:04:25 +0530

    Helped optimise the code

--- C++/brick-wall.cpp
+++ C++/brick-wall.cpp
@@ -26,3 +26,22 @@ public:
         return (n-maxcount);
     }
 };
+
+Reduced Code for the given problem that reduces one for loop and also has less auxiliary space!
+
+// Time:  O(n), n is the total number of the bricks
+// Space: O(m), m is the total number different widths
+
+class Solution {
+public:
+    int leastBricks(vector<vector<int>>& wall) {
+        unordered_map<int, int> widths;
+        auto result = wall.size();
+        for (const auto& row : wall) {
+            for (auto i = 0, width = 0; i < row.size() - 1; ++i) {
+                result = min(result, wall.size() - (++widths[width += row[i]]));
+            }
+        }
+        return result;
+    }
+};

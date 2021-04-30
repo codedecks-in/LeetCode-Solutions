d3e4d25c3e646b45cf48e5413cc7e53324240a24
G@rv!T <61387258+garvitdoda1266@users.noreply.github.com>
2021-04-30 14:01:33 +0530

    Remove-Covered-Intervals.cpp (#163)
    * improved c++ code
    
    * Improved code with O(nlogn) solution
    
    Co-authored-by: Gourav Rusiya <gouravrusiya.lnct@gmail.com>

--- C++/Remove-Covered-Intervals.cpp
+++ C++/Remove-Covered-Intervals.cpp
@@ -27,3 +27,32 @@ public:
 };
 
 //Complexity: O(n*n)
+
+
+
+SIMPLIFIED SOLUTION in O(nlogn) time
+
+//
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),[](vector<int>& v1,vector<int>& v2){
+            if(v1[0]==v2[0])
+                return v1[1]>v2[1];
+            return v1[0]<v2[0];
+        });
+        int end = intervals[0][1];
+        int sz = 1;
+        for(int i=1;i<intervals.size();i++)
+        {
+            if(intervals[i][1]>end)
+            {
+                end = intervals[i][1];
+                sz++;
+            }
+        }
+        return sz;
+    }
+};
+
+//

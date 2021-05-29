68163cc8e4d19dfb50b5375d8e589ad35c400c5f
Moulina Pradhan <pmoulina76@gmail.com>
2021-05-29 22:36:21 +0530

    Evaluate reverse polish notation (#171)
    * Solution of Evaluate Reverse Polish Notation
    
    * Update evaluate_reverse_polish_notation.java
    
    * update readme.md

---
+++ Java/evaluate_reverse_polish_notation.java
@@ -0,0 +1,30 @@
+
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        int a,b;
+        for(String s: tokens){
+            if(s.equals("+")){
+                st.add(st.pop()+st.pop());
+            }
+            else if(s.equals("*")){
+                st.add(st.pop() * st.pop());
+            }
+            else if(s.equals("/")){
+                b=st.pop();
+                a = st.pop();
+                st.add(a/b);
+            }
+            else if(s.equals("-")){
+                b = st.pop();
+                a = st.pop();
+                st.add(a-b);
+            }
+            else{
+                st.add(Integer.parseInt(s));
+            }
+        }
+        return st.pop();
+    }
+}

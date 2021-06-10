67569290149022ed4e28bbee2e7298f1de4148d3
Krishanus-2001 <krishanu21saini@gmail.com>
2021-06-10 16:23:27 +0530

    Fix xor-operation-in-an-array

--- C++/xor-operation-in-an-array.cpp
+++ C++/xor-operation-in-an-array.cpp
@@ -1,6 +1,8 @@
+// difficulty easy
 class Solution {
 public:
     int xorOperation(int n, int start) {
+        // brute force method
         int ans = 0;
         for(int i=0;i<n;i++){
             ans ^= (start + 2*i);

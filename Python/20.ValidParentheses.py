59c34fb2aac78dac8e3e4400b0972b5ca6092536
pranav-git-hub <66816957+pranav-git-hub@users.noreply.github.com>
2021-10-01 16:30:58 +0530

    Create 20.ValidParentheses.py

---
+++ Python/20.ValidParentheses.py
@@ -0,0 +1,19 @@
+'''
+Space complexity = n
+Time complexity = n
+'''
+
+class Solution:
+    # @return a boolean
+    def isValid(self, s):
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
+            else:
+                return False
+        return stack == []

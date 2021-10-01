b24e68d6a86b81d58927aa934d6c2199dffa370a
pranav-git-hub <66816957+pranav-git-hub@users.noreply.github.com>
2021-10-01 21:52:36 +0530

    20. Valid Parentheses (#196)
    * Create 20.ValidParentheses.py
    
    * Create 20.ValidParentheses.java
    
    * Delete 20.ValidParentheses.py
    
    * Update 20.ValidParentheses.java
    
    * Update README.md

---
+++ Java/20.ValidParentheses.java
@@ -0,0 +1,16 @@
+//Time Complexity n
+//Space Complexity n
+public boolean isValid(String s) {
+	Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray()) {
+		if (c == '(')
+			stack.push(')');
+		else if (c == '{')
+			stack.push('}');
+		else if (c == '[')
+			stack.push(']');
+		else if (stack.isEmpty() || stack.pop() != c)
+			return false;
+	}
+	return stack.isEmpty();
+}

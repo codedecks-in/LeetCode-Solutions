4dca7967070b4957926610f52202cecd86b8ec9b
Vatsal Patel <75309827+vatsal-dp@users.noreply.github.com>
2021-10-18 11:53:57 +0530

    41. First Missing Positive question of Leetcode

---
+++ Java/FirstMissingPositive.java
@@ -0,0 +1,35 @@
+/*
+Question:
+Given an unsorted integer array nums, return the smallest missing positive integer.
+You must implement an algorithm that runs in O(n) time and uses constant extra space.
+ */
+
+package com.vatsal;
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i]>0 && nums[i]<=nums.length && nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+        int ans = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                ans = j + 1;
+                return ans;
+            }
+        }
+        return nums.length+1;
+    }
+
+    static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}

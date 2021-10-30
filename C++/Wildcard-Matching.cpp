99551da8810921ee07b05768b5f73c69a0f476e7
Yashashree-Navander <yashnavander06@gmail.com>
2021-10-30 18:27:28 +0530

    Update Wildcard-Matching.cpp

--- C++/Wildcard-Matching.cpp
+++ C++/Wildcard-Matching.cpp
@@ -1,3 +1,11 @@
+/*
+Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+*/
+
 class Solution {
 public:
     bool isMatch(string &s, string &p) {

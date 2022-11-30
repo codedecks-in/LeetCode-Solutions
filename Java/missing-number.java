779b979570ab1743ae6d99e006db29b248337584
Swapnil Jadhav <89910173+Swapnil-2503@users.noreply.github.com>
2022-12-01 00:00:23 +0530

    Update missing-number.java

--- Java/missing-number.java
+++ Java/missing-number.java
@@ -36,4 +36,18 @@ class Solution {
         
         return xor;
     }
+    
+    //using quick sort
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int a=0;
+         for(int i=0;i<nums.length;i++){
+              if(nums[a]!=i){
+              return i;
+               }
+             a++;
+           }
+         return nums.length;
+    }
+
 }

28811104056e53fef1eedd6f06373e2a382741dd
V ANIRRUTH <74400297+anirruth0312@users.noreply.github.com>
2023-07-13 16:08:37 +0530

    Update Linked-List-Cycle-II.cpp
    Solution using hashtable

--- C++/Linked-List-Cycle-II.cpp
+++ C++/Linked-List-Cycle-II.cpp
@@ -1,33 +1,26 @@
-
-class Solution
-{
-	public:
-		ListNode* detectCycle(ListNode *head)
-		{
-			//If head is null or head->next is null dont do anything just return.
-			if (!head) return NULL;
-			if (head->next == NULL) return NULL;
-			//Take two pointers slow and fast both initially pointing to head .  
-			ListNode *fast = head;
-			ListNode *slow = head;
-			while (fast and fast->next)
-			{
-				// Move fast pointer two step and slow pointer one step forward.
-				fast = fast->next->next;
-				slow = slow->next;
-				//At some time if fast and slow are equal it means there is a cycle .
-				if (fast == slow) break;
-			}
-
-			if (fast == NULL or fast->next == NULL) return NULL;
-			//Now we point one pointer to head and increment both  the pointers one step untill slow and fast pointer do not become equal.
-			slow = head;
-			while (slow != fast)
-			{
-				slow = slow->next;
-				fast = fast->next;
-			}
-
-			return slow;
-		}
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool cycleDetect(ListNode* head) {
+    unordered_set<ListNode*> hashTable;
+    while(head != NULL) {
+        if(hashTable.find(head) != hashTable.end()) 
+        {
+            return true;
+        }
+        hashTable.insert(head);
+        head = head->next;
+    }
+    return false;
+    }
+    bool hasCycle(ListNode *head) {
+        return cycleDetect(head);
+    }
 };

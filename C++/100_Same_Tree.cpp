227367b166dba434a3d34a68d06f52f3c9b076a5
Nashit Budhwani <nashitnoorali78@gmail.com>
2023-09-12 01:07:52 +0500

    A better C++ solution for problem 100:

--- C++/100_Same_Tree.cpp
+++ C++/100_Same_Tree.cpp
@@ -1,27 +1,8 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == nullptr && q == nullptr)
-            return true;
-        if(p == nullptr || q == nullptr) 
-            return false;
-        if(p->val != q->val) return false;
-        
-        return (isSameTree(p->right,q->right) && isSameTree(p->left, q->left));
+        if (!p && !q) return true;
+        if (!p || !q || p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
-
-// Complexity Analysis:
-// Time complexity : O(N), where N is a number of nodes in the tree, since one visits each node exactly once.
-// Space complexity :O(log(N)) in the best case of completely balanced tree and O(N) in the worst case of completely unbalanced tree.

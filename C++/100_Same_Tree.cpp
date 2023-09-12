bdf1a695868d44af551908b21c8a3ddfc811c0ae
Nashit Budhwani <nashitnoorali78@gmail.com>
2023-09-12 14:46:20 +0500

    More Compact Solution

--- C++/100_Same_Tree.cpp
+++ C++/100_Same_Tree.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p && !q) return true;
-        if (!p || !q || p->val != q->val) return false;
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        return (!p && !q) || (p && q && p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     }
 };

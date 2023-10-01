ead44b6175cb168507ca3109885b4fa6234fce75
Ankush Tripathi <89534251+ankushtripathii@users.noreply.github.com>
2023-10-01 14:18:31 +0530

    Update Minimum-Operations-to-Collect-Elements.java

--- Java/Minimum-Operations-to-Collect-Elements.java
+++ Java/Minimum-Operations-to-Collect-Elements.java
@@ -1,5 +1,5 @@
 /*
-2869. Minimum Operations to Collect Elements
+2869. Minimum Operations to Collect Elements    difficulty: easy
 
 problem definition: You are given an array nums of positive integers and an integer k.
 In one operation, you can remove the last element of the array and add it to your collection.
@@ -8,6 +8,8 @@ Return the minimum number of operations needed to collect elements 1, 2, ..., k.
 Input: nums = [3,1,5,4,2], k = 2
 Output: 4
 Explanation: After 4 operations, we collect elements 2, 4, 5, and 1, in this order. Our collection contains elements 1 and 2. Hence, the answer is 4.
+
+Time complexity :O(N)
 
 
 

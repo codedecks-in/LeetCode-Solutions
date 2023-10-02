63c35cfeedd88d96997b512604933539611fa580
Niyati Soni <85472780+Niyati3031@users.noreply.github.com>
2023-10-02 09:53:57 +0530

    Create RangeSumOfBST.cpp

---
+++ C++/RangeSumOfBST.cpp
@@ -0,0 +1,36 @@
+// Question:- Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void solve(TreeNode* root,int low,int high,int &ans){
+        if(!root) return;
+        if(root-> val >=low && root->val <=high){ //checking if root value is in range
+            ans+=root->val;
+            solve(root->left, low, high, ans);  //recursively calling left tree
+            solve(root->right, low, high, ans);  //recursively calling right tree
+        }
+        else if(root->val> high){   //if root value is more than high, then according to BST low values won't be in right tree so search left tree
+            solve(root->left, low, high, ans);
+        }
+        else if(root->val < low){ //if root value is less than low, then according to BST low values won't be in left tree so search right tree
+            solve(root->right, low, high, ans);
+        }
+    }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum = 0;
+        solve(root, low, high, sum);
+        return sum;
+    }
+};

17f894e6146ebcfb141e05f5b3cd92b19658c31c
Tejo <tejokaushal001@gmail.com>
2023-10-07 11:19:57 +0530

    made the documentation changes

--- C++/Trapping-Rain-Water.cpp
+++ C++/Trapping-Rain-Water.cpp
@@ -1,27 +1,30 @@
-class Solution {
-public:
-    int trap(vector<int>& h) {
-        int total = 0, l = 0, r = h.size() - 1,
-            left_max = h[l], right_max = h[r];
-        
-        while(l + 1 < r)
-        {
-            if (h[l] <= h[r])
-            {
-                ++l;
-                total += max(0, left_max - h[l]);
-                left_max = max(left_max, h[l]);
-            } else {
-                --r;
-                total += max(0, right_max - h[r]);
-                right_max = max(right_max, h[r]);
-            }
-        }
-
-        return total;    
-    }
-};
+//https://leetcode.com/problems/trapping-rain-water/
+//Difficulty Level: Hard 
 
 /*Complexity
 Time complexity:O(n) Space complexity:O(1)
 */
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int total = 0;                  // Initialize the variable to store the total trapped water
+        int l = 0, r = h.size() - 1;    // Initialize left and right pointers
+        int left_max = h[l];            // Initialize the maximum height from the left to the first element
+        int right_max = h[r];           // Initialize the maximum height from the right to the last element
+        
+        while (l + 1 < r) {             // Continue while there are at least 3 elements between left and right pointers
+            if (h[l] <= h[r]) {         // If the height at the left pointer is less than or equal to the right pointer
+                ++l;                    // Move the left pointer to the right
+                total += max(0, left_max - h[l]);  // Calculate trapped water on the left side
+                left_max = max(left_max, h[l]);    // Update the maximum height from the left
+            } else {                    // If the height at the left pointer is greater than the right pointer
+                --r;                    // Move the right pointer to the left
+                total += max(0, right_max - h[r]); // Calculate trapped water on the right side
+                right_max = max(right_max, h[r]);  // Update the maximum height from the right
+            }
+        }
+
+        return total; // Return the total trapped water    
+    }
+};
+

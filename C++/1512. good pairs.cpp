25324fc05cce0d402eea5591b994776eba774c83
Sidd7000 <146730223+Sidd7000@users.noreply.github.com>
2023-10-17 09:26:43 +0530

    Create 1512. good pairs.cpp
    Solved issue #439

---
+++ C++/1512. good pairs.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int numIdenticalPairs(vector<int>& nums) {
+    unordered_map<int, int> count;
+
+    for (int num : nums) {
+        count[num]++;
+    }
+
+    int goodPairs = 0;
+    for (const auto& pair : count) {
+        int occurrences = pair.second;
+        if (occurrences >= 2) {
+            goodPairs += (occurrences * (occurrences - 1)) / 2;
+        }
+    }
+
+    return goodPairs;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1, 1, 3, 4};
+    int result = numIdenticalPairs(nums);
+    cout << "Number of Good Pairs: " << result << endl;
+
+    return 0;
+}

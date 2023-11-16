1f3b3003de452cf182d0af4ca7676302d5f239f1
sricharan200 <148656478+sricharan200@users.noreply.github.com>
2023-11-16 13:32:43 -0500

    Update Linked_List_Cycle.py

--- Python/Linked_List_Cycle.py
+++ Python/Linked_List_Cycle.py
@@ -11,14 +11,23 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # Return False if the list is empty or has only one node (no cycle possible)
         if not head or not head.next:
             return False
         
+        # Initialize two pointers: 'slow' moves one step at a time, 'fast' moves two steps
         slow = head
         fast = head.next
+
+        # Iterate through the list
         while slow != fast:
+            # If 'fast' reaches the end of the list (or its next step is None), there's no cycle
             if not fast or not fast.next:
                 return False
+
+            # Move 'slow' one step forward and 'fast' two steps forward
             slow = slow.next
             fast = fast.next.next
+
+        # If 'slow' and 'fast' meet, there is a cycle in the list
         return True

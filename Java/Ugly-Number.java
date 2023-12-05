c77dc5d23c4696beac9a6111ffc2678c3d579813
anelatrakic <79427113+anelatrakic@users.noreply.github.com>
2023-12-05 17:07:21 -0500

    Adding a second solution to Ugly-Number.java

--- Java/Ugly-Number.java
+++ Java/Ugly-Number.java
@@ -21,3 +21,19 @@ class Solution {
         return false;
     }
 }
+
+/* adding another solution without recursion */
+class Solution {
+    public boolean isUgly(int num) {
+        if (num <= 0) return false;
+        int[] divisors = {2, 3, 5};
+
+        for (int divisor : divisors) {
+            while (num % divisor == 0) {
+                num /= divisor;
+            }
+        }
+
+        return num == 1;
+    }
+}
